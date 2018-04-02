347331b005ceb9f2cd1218339ced18f7c58ec561
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-04-02 04:53:22 -0500

    Create App Routes Using React-Router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './style/index.css';
-import App from './components/App';
+import Register from './components/Register.jsx';
+import Login from './components/Login.jsx';
+import NotFound from './components/404.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Register} />
+            <Route path="/login" component={Login} />
+            <Route component={NotFound} />
+        </Switch>
+    </BrowserRouter>
+    , document.getElementById('root'));
 registerServiceWorker();
