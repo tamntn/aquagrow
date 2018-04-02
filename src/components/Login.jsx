17809c51ca8e9aaa42b4294e9f1c314ada2f83ac
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-04-02 17:12:26 -0500

    Update Login Form Component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,7 @@ import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import { Card } from 'antd';
 import '../style/Login.css';
 import logo from '../images/logo/small_01.png';
+import BackgroundImage from './BackgroundImage.jsx';
 const FormItem = Form.Item;
 
 class NormalLoginForm extends Component {
@@ -19,43 +20,46 @@ class NormalLoginForm extends Component {
     render() {
         const { getFieldDecorator } = this.props.form;
         return (
-            <div className='login-container'>
-                <Card style={{ width: 350 }} className="card-container">
-                    <div className='card-header'>
-                        {/* <h1>Welcome</h1> */}
-                        <img src={logo} height="64" width="64" />
-                        <br></br><br></br>
-                    </div>
-                    <Form onSubmit={this.handleSubmit} className="login-form">
-                        <FormItem>
-                            {getFieldDecorator('userName', {
-                                rules: [{ required: true, message: 'Please input your username!' }],
-                            })(
-                                <Input size="large" prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.5)' }} />} placeholder="Username" />
-                            )}
-                        </FormItem>
-                        <FormItem>
-                            {getFieldDecorator('password', {
-                                rules: [{ required: true, message: 'Please input your Password!' }],
-                            })(
-                                <Input size="large" prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.5)' }} />} type="password" placeholder="Password" />
-                            )}
-                        </FormItem>
-                        <FormItem>
-                            {getFieldDecorator('remember', {
-                                valuePropName: 'checked',
-                                initialValue: true,
-                            })(
-                                <Checkbox>Remember me</Checkbox>
-                            )}
-                            <a className="login-form-forgot" href="">Forgot password?</a>
-                            <Button size="large" type="primary" htmlType="submit" className="login-form-button">
-                                Log in
+            <div>
+                <BackgroundImage />
+                <div className='login-container'>
+                    <Card style={{ width: 350 }} className="card-container">
+                        <div className='card-header'>
+                            {/* <h1>Welcome</h1> */}
+                            <img src={logo} height="64" width="64" />
+                            <br></br><br></br>
+                        </div>
+                        <Form onSubmit={this.handleSubmit} className="login-form">
+                            <FormItem>
+                                {getFieldDecorator('userName', {
+                                    rules: [{ required: true, message: 'Please input your username!' }],
+                                })(
+                                    <Input size="large" prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.5)' }} />} placeholder="Username" />
+                                )}
+                            </FormItem>
+                            <FormItem>
+                                {getFieldDecorator('password', {
+                                    rules: [{ required: true, message: 'Please input your Password!' }],
+                                })(
+                                    <Input size="large" prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.5)' }} />} type="password" placeholder="Password" />
+                                )}
+                            </FormItem>
+                            <FormItem>
+                                {getFieldDecorator('remember', {
+                                    valuePropName: 'checked',
+                                    initialValue: true,
+                                })(
+                                    <Checkbox>Remember me</Checkbox>
+                                )}
+                                <a className="login-form-forgot" href="">Forgot password?</a>
+                                <Button size="large" type="primary" htmlType="submit" className="login-form-button">
+                                    Log in
           					</Button>
-                            Or <Link to="/">register now!</Link>
-                        </FormItem>
-                    </Form>
-                </Card>
+                                Or <Link to="/">register now!</Link>
+                            </FormItem>
+                        </Form>
+                    </Card>
+                </div>
             </div>
         );
     }
