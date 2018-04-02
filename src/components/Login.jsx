c2eeb73141b8057be4ca03d390493a2897e21a75
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-04-01 22:59:26 -0500

    Add Login Form Component

---
+++ src/components/Login.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { Card } from 'antd';
+import '../style/Login.css';
+import logo from '../images/logo/small_01.png'
+const FormItem = Form.Item;
+
+class NormalLoginForm extends React.Component {
+    handleSubmit = (e) => {
+        // e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    }
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <div className='register-container'>
+                <Card style={{ width: 350 }} className="card-container">
+                    <div className='card-header'>
+                        {/* <h1>Welcome</h1> */}
+                        <img src={logo} height="64" width="64" />
+                        <br></br><br></br>
+                    </div>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <FormItem>
+                            {getFieldDecorator('userName', {
+                                rules: [{ required: true, message: 'Please input your username!' }],
+                            })(
+                                <Input size="large" prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.5)' }} />} placeholder="Username" />
+                            )}
+                        </FormItem>
+                        <FormItem>
+                            {getFieldDecorator('password', {
+                                rules: [{ required: true, message: 'Please input your Password!' }],
+                            })(
+                                <Input size="large" prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.5)' }} />} type="password" placeholder="Password" />
+                            )}
+                        </FormItem>
+                        <FormItem>
+                            {getFieldDecorator('remember', {
+                                valuePropName: 'checked',
+                                initialValue: true,
+                            })(
+                                <Checkbox>Remember me</Checkbox>
+                            )}
+                            <a className="login-form-forgot" href="">Forgot password?</a>
+                            <Button size="large" type="primary" htmlType="submit" className="login-form-button">
+                                Log in
+          					</Button>
+                            Or <a href="">register now!</a>
+                        </FormItem>
+                    </Form>
+                </Card>
+            </div>
+        );
+    }
+}
+
+const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
+
+export default WrappedNormalLoginForm;
