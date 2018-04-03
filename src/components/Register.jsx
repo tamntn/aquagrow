f8a344b778dafd8013fc6e1b277f56334f5b7377
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-04-03 03:23:50 -0500

    Add Register Component

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Carousel } from 'antd';
 import '../style/Register.css';
 import logo from '../images/logo/small_01.png';
+import RegisterForm from './RegisterForm.jsx';
 
 class RegisterCarousel extends Component {
     render() {
         return (
             <div className="register-container">
-                <div className="left">
+                <div className="register-left">
                     <Carousel effect="fade" autoplay>
                         <div className="slide1"></div>
                         <div className="slide2"></div>
                         <div className="slide3"></div>
                     </Carousel>
                 </div>
-                <div className="right">
+                <div className="register-right">
                     <div className="register-logo-container">
-                        <img src={logo} height="64" width="64" />
+                        <Link to="/">
+                            <img src={logo} alt="" height="64" width="64" />
+                        </Link>
+                    </div>
+                    <div className="register-form-container">
+                        <h1>Welcome to AquaGrow</h1>
+                        <br></br>
+                        <RegisterForm />
+                        Already have an account? <Link to="/login">Log in</Link>
+                        <br></br>
+                        <br></br>
                     </div>
                 </div>
             </div>
---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,132 @@
+import React from 'react';
+import axios from 'axios';
+import { Card, Form, Input, Tooltip, Icon, Checkbox, Button } from 'antd';
+const FormItem = Form.Item;
+
+class RegistrationForm extends React.Component {
+    state = {
+        confirmDirty: false,
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+                axios.post('https://aquagrow.life/api/user', values)
+                    .then((result) => {
+                        console.log(result);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    })
+            }
+        });
+    }
+
+    handleConfirmBlur = (e) => {
+        const value = e.target.value;
+        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+    }
+
+    compareToFirstPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('Two passwords that you enter is inconsistent!');
+        } else {
+            callback();
+        }
+    }
+
+    validateToNextPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['confirm'], { force: true });
+        }
+        callback();
+    }
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+
+        return (
+            <Card className="card-container">
+                <Form onSubmit={this.handleSubmit}>
+                    <FormItem>
+                        {getFieldDecorator('name', {
+                            rules: [{ required: true, message: 'Please input your full name!' }],
+                        })(
+                            <Input size="large" prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.5)' }} />} placeholder="Full Name" />
+                        )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator('username', {
+                            rules: [{ required: true, message: 'Please input your username!' }],
+                        })(
+                            <Input size="large" prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.5)' }} />} placeholder="Username" />
+                        )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator('password', {
+                            rules: [{
+                                required: true, message: 'Please input your password!',
+                            }, {
+                                validator: this.validateToNextPassword,
+                            }],
+                        })(
+                            <Input size="large" prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.5)' }} />} type="password" placeholder="Password" />
+                        )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator('confirm', {
+                            rules: [{
+                                required: true, message: 'Please confirm your password!',
+                            }, {
+                                validator: this.compareToFirstPassword,
+                            }],
+                        })(
+                            <Input size="large" onBlur={this.handleConfirmBlur} prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.5)' }} />} type="password" placeholder="Confirm Password" />
+                        )}
+                    </FormItem>
+                    {/* <FormItem
+                        label={(
+                            <span>
+                                Nickname&nbsp;
+                                <Tooltip title="What do you want others to call you?">
+                                    <Icon type="question-circle-o" />
+                                </Tooltip>
+                            </span>
+                        )}
+                    >
+                        {getFieldDecorator('nickname', {
+                            rules: [{ required: true, message: 'Please input your nickname!', whitespace: true }],
+                        })(
+                            <Input size="large" />
+                        )}
+                    </FormItem> */}
+                    {/* <FormItem>
+                        {getFieldDecorator('phone', {
+                            rules: [{ required: true, message: 'Please input your phone number!' }],
+                        })(
+                            <Input size="large" addonBefore="+1" prefix={<Icon type="phone" style={{ color: 'rgba(0,0,0,.5)' }} />} placeholder="Phone" />
+                        )}
+                    </FormItem> */}
+                    {/* <FormItem>
+                        {getFieldDecorator('agreement', {
+                            valuePropName: 'checked',
+                        })(
+                            <Checkbox>I have read the <a href="">agreement</a></Checkbox>
+                        )}
+                    </FormItem> */}
+                    <FormItem>
+                        <Button size="large" type="primary" htmlType="submit" className="register-form-button">Register</Button>
+                    </FormItem>
+                </Form>
+            </Card>
+        );
+    }
+}
+
+const WrappedRegistrationForm = Form.create()(RegistrationForm);
+
+export default WrappedRegistrationForm;
