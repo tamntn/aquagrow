a02db1f97cce78f609c9692a284197e989e20f69
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-04-04 06:47:05 -0500

    Add Redux Dependencies  and Store To Main App

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
+
 import './style/index.css';
 import Register from './components/Register.jsx';
-import Login from './components/Login.jsx';
-import Dashboard from './components/Dashboard.jsx';
+import Login from './containers/Login.jsx';
+import Dashboard from './containers/Dashboard.jsx';
 import NotFound from './components/404.jsx';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
-            <Route component={NotFound} />
-        </Switch>
-    </BrowserRouter>
+    <Provider store={createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Dashboard} />
+                <Route path="/register" component={Register} />
+                <Route path="/login" component={Login} />
+                <Route component={NotFound} />
+            </Switch>
+        </BrowserRouter>
+    </Provider>
     , document.getElementById('root'));
 registerServiceWorker();
