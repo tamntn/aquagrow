1fae218478ab8d7e92ce9461144f59f3b8a54b81
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-04-07 05:27:56 -0500

    Created Config File

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+export const apiRoutes = {
+    rootUrl: 'https://aquagrow.life/api'
+}
+
+export const actionType = {
+    FETCH_USER: 'fetch_user',
+    REGISTER: 'register',
+    AUTHENTICATE: 'authenticate',
+    LOGOUT: 'logout',
+    CLEAR_ERROR: 'clear_error'
+}
