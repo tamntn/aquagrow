7c1c5f570cdeb48a32bd7088e1c4c81176719e1d
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-04-07 03:37:51 -0500

    Welcome Page All Steps Done

--- src/containers/Welcome.jsx
+++ src/containers/Welcome.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Redirect, withRouter } from 'react-router-dom';
 import { Form, Card, Steps, Button, Icon, Input, Upload, message } from 'antd';
 import axios from 'axios';
 import BackgroundImage from '../components/BackgroundImage.jsx';
+import { fetchUser } from '../actions/action_user';
 import '../style/Welcome.css';
 const Step = Steps.Step;
 const FormItem = Form.Item;
@@ -22,9 +25,20 @@ class Welcome extends Component {
             zipCode: '',
             growZone: '',
             phone: '',
-            picture: '',
+            pictureUrl: '',
+            fileList: [],
+            systemName: '',
+            submitting: false,
         };
         this.checkGrowZone = this.checkGrowZone.bind(this);
+        this.checkSystemName = this.checkSystemName.bind(this);
+    }
+
+    componentWillMount() {
+        const currentUser = localStorage.getItem('username');
+        if (currentUser) {
+            this.props.fetchUser(currentUser);
+        }
     }
 
     componentDidUpdate() {
@@ -47,21 +61,71 @@ class Welcome extends Component {
                     if (this.state.zipCode) {
                         actionButton.removeAttribute("disabled");
                     }
+                    break;
                 case 2:
                     if (this.state.phone) {
                         actionButton.removeAttribute("disabled");
                     }
+                    break;
                 case 3:
-                    if (this.state.picture) {
+                    if (this.state.fileList.length === 1 && this.state.systemName) {
                         actionButton.removeAttribute("disabled");
                     }
+                    break;
             }
         }
     }
 
+    handleSubmit() {
+        const { fileList } = this.state;
+        const formData = new FormData();
+        fileList.forEach((file) => {
+            formData.append('files[]', file);
+        });
+
+        this.setState({
+            submitting: true,
+        });
+
+        axios.post('http://localhost:8080/api/upload', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then((res) => {
+            // We need to update user info
+            const pictureUrl = res.data.secure_url;
+            const updateValues = {
+                zipCode: this.state.zipCode,
+                zone: this.state.growZone,
+                phone: this.state.phone,
+                pictureUrl
+            }
+
+            return axios.put(`http://localhost:8080/api/user/${localStorage.getItem('username')}`, updateValues)
+        }).then((res) => {
+            // And create a new system and push that system into the user
+            message.success('User information has successfully updated 🎉');
+            return axios.post('http://localhost:8080/api/system', {
+                username: localStorage.getItem('username'),
+                systemName: this.state.systemName
+            })
+        }).then((res) => {
+            message.success('New system has been initiated 🐟☘️');
+            this.setState({
+                submitting: false,
+            });
+            this.props.setUserInitialSetupToTrue();
+        }).catch((err) => {
+            this.setState({
+                submitting: false,
+            });
+            message.error('Failed to submit form. Please try again 🤭');
+        })
+    }
+
     checkGrowZone(rule, value, callback) {
         if (value && value.length == 5) {
-            axios.get(`https://aquagrow.life/api/zone/${value}`)
+            axios.get(`https://aquagrow.life/api/zone/zip/${value}`)
                 .then((res) => {
                     if (res.data.error) {
                         this.setState({
@@ -80,7 +144,7 @@ class Welcome extends Component {
                         callback();
                     }
                 })
-        } else if (value.length > 5) {
+        } else if (value) {
             this.setState({
                 disabled: true,
                 zipCode: '',
@@ -88,11 +152,6 @@ class Welcome extends Component {
             });
             callback('Please provide a 5-digit zip code!');
         } else {
-            this.setState({
-                disabled: true,
-                zipCode: '',
-                growZone: ''
-            });
             callback();
         }
     }
@@ -115,6 +174,24 @@ class Welcome extends Component {
         }, 100);
     }
 
+    checkSystemName(rule, value, callback) {
+        if (value && value.length > 5) {
+            this.setState({
+                disabled: false,
+                systemName: value
+            })
+            callback();
+        } else if (value) {
+            this.setState({
+                disabled: true,
+                systemName: ''
+            })
+            callback('System name is too short!');
+        } else {
+            callback();
+        }
+    }
+
     next() {
         const current = this.state.current + 1;
         this.setState({
@@ -133,13 +210,40 @@ class Welcome extends Component {
 
     render() {
         const { current } = this.state;
+        const { submitting } = this.state;
         const { getFieldDecorator } = this.props.form;
         const growZone = this.state.growZone ? (
             <h4>✅ Your grow zone is: {this.state.growZone}</h4>
         ) : (
                 <h4></h4>
             )
+
+        const uploadProps = {
+            action: 'http://localhost:8080/api/upload',
+            onRemove: (file) => {
+                this.setState(({ fileList }) => {
+                    const index = fileList.indexOf(file);
+                    const newFileList = fileList.slice();
+                    newFileList.splice(index, 1);
+                    return {
+                        fileList: newFileList,
+                    };
+                });
+            },
+            beforeUpload: (file) => {
+                this.setState(({ fileList }) => ({
+                    fileList: [...fileList, file],
+                }));
+                return false;
+            },
+            fileList: this.state.fileList,
+            type: "file",
+            accept: "image/*",
+            multiple: false
+        };
+
         const currentJWT = localStorage.getItem('jwt');
+        let redirectComponent;
 
         const steps = [{
             title: 'Welcome',
@@ -164,7 +268,9 @@ class Welcome extends Component {
                 <div className="steps-content-wrapper">
                     <div className="welcome-left-container">
                         <h3>Provide your zipcode</h3>
-                        <FormItem>
+                        <FormItem
+                            hasFeedback
+                        >
                             {getFieldDecorator('zipCode', {
                                 rules: [{
                                     required: true, message: 'Please input your zip code!'
@@ -197,7 +303,9 @@ class Welcome extends Component {
                 <div className="steps-content-wrapper">
                     <div className="welcome-left-container">
                         <h3>Provide your phone number</h3>
-                        <FormItem>
+                        <FormItem
+                            hasFeedback
+                        >
                             {getFieldDecorator('phone', {
                                 rules: [{
                                     required: true, message: 'Please input your phone number!'
@@ -221,20 +329,39 @@ class Welcome extends Component {
                     </div>
                 </div>
             ),
-            icon: (<div className="welcome-icon-container"><Icon type="notification" /></div>)
+            icon: (<div className="welcome-icon-container"><Icon type="bell" /></div>)
         }, {
             title: 'Profile',
             content: (
                 <div className="steps-content-wrapper">
                     <div className="welcome-left-container">
-                        <h3>Upload your profile picture</h3>
-                        <Upload>
-                            <Button>
-                                <Icon type="upload" /> Click to Upload
+                        <h3>Customize your profile</h3>
+                        <FormItem>
+                            <Upload {...uploadProps}>
+                                <Button
+                                    size="large"
+                                    disabled={this.state.fileList.length === 1}
+                                >
+                                    <Icon type="upload" /> Select Profile Picture
                             </Button>
-                        </Upload>
+                            </Upload>
+                        </FormItem>
+                        <FormItem
+                            hasFeedback
+                        >
+                            {getFieldDecorator('systemName', {
+                                rules: [{
+                                    required: true, message: 'Please name your aquaponics system!'
+                                }, {
+                                    validator: this.checkSystemName
+                                }],
+                                initialValue: this.state.systemName
+                            })(
+                                <Input size="large" placeholder="System Name" />
+                            )}
+                        </FormItem>
                         <div className="welcome-description">
-                            <p>Let's personalize your account and have fun!</p>
+                            <p>Final step to make the system truly yours!</p>
                         </div>
                     </div>
                     <div className="welcome-right-container">
@@ -250,60 +377,88 @@ class Welcome extends Component {
                 <Steps size="small" current={current}>
                     {steps.map(item => <Step key={item.title} title={item.title} icon={item.icon} />)}
                 </Steps>
-            </div >
+            </div>
         )
 
         // Redirect back to login page if not logged in
         if (!currentJWT) {
             message.destroy();
             message.error('Please log in first to access your AquaGrow account 😇');
-            return <Redirect to='/login' />;
+            redirectComponent = (<Redirect to='/login' />)
         }
+        // else if (this.props.user) {
+        //     if (this.props.user.zipCode && this.props.user.phone) {
+        //         redirectComponent = (<Redirect to='/' />)
+        //     }
+        // }
 
         return (
             <div>
-                <BackgroundImage />
-                <div className="welcome-container">
-                    <Card loading={false} title={stepsComponent} className="welcome-card">
-                        <Form onSubmit={this.handleSubmit}>
-                            <div className="steps-content">{steps[this.state.current].content}</div>
-                            <div className="steps-action">
-                                {
-                                    this.state.current === 0
-                                    &&
-                                    <div className="button-previous">
+                {currentJWT ? (
+                    <div>
+                        <BackgroundImage />
+                        <div className="welcome-container">
+                            <Card loading={false} title={stepsComponent} className="welcome-card">
+                                <Form onSubmit={this.handleSubmit}>
+                                    <div className="steps-content">{steps[this.state.current].content}</div>
+                                    <div className="steps-action">
+                                        {
+                                            this.state.current === 0
+                                            &&
+                                            <div className="button-previous">
+                                            </div>
+                                        }
+                                        {
+                                            this.state.current > 0
+                                            &&
+                                            <div className="button-previous">
+                                                <Button size="large" onClick={() => this.prev()}><Icon type="left" />
+                                                    Go back
+                                            </Button>
+                                            </div>
+                                        }
+                                        {
+                                            this.state.current < steps.length - 1
+                                            &&
+                                            <div className="button-next">
+                                                <Button id="nextButton" size="large" type="primary" onClick={() => this.next()}>
+                                                    Continue<Icon type="right" />
+                                                </Button>
+                                            </div>
+                                        }
+                                        {
+                                            this.state.current === steps.length - 1
+                                            &&
+                                            <div className="button-next">
+                                                <Button id="submitButton" size="large" type="primary" onClick={() => this.handleSubmit()} loading={submitting}>
+                                                    {submitting ? 'Processing' : 'Finish!'}
+                                                </Button>
+                                            </div>
+                                        }
                                     </div>
-                                }
-                                {
-                                    this.state.current > 0
-                                    &&
-                                    <div className="button-previous">
-                                        <Button size="large" onClick={() => this.prev()}><Icon type="left" />Go Back</Button>
-                                    </div>
-                                }
-                                {
-                                    this.state.current < steps.length - 1
-                                    &&
-                                    <div className="button-next">
-                                        <Button id="nextButton" size="large" type="primary" onClick={() => this.next()}>Continue<Icon type="right" /></Button>
-                                    </div>
-                                }
-                                {
-                                    this.state.current === steps.length - 1
-                                    &&
-                                    <div className="button-next">
-                                        <Button id="submitButton" size="large" type="primary" onClick={() => message.success('Processing complete!')}>Go To Your AquaGrow!</Button>
-                                    </div>
-                                }
-                            </div>
-                        </Form>
-                    </Card>
-                </div>
+                                </Form>
+                            </Card>
+                        </div>
+                    </div>
+                ) : (
+                        <Redirect to='/login' />
+                    )}
             </div>
         );
     }
 }
 
+function mapStateToProps(state) {
+    return {
+        loggedIn: state.loggedIn,
+        user: state.user
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchUser }, dispatch);
+}
+
 const WrappedWelcomeForm = Form.create()(Welcome);
 
-export default WrappedWelcomeForm;
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(WrappedWelcomeForm));
