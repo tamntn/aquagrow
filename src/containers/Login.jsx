57bdfc000e74b7fd700d5a436cf6e8e80b68b351
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-04-08 22:43:35 -0500

    Modulize Login Page

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,121 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { authenticate, clearError } from '../actions/action_user';
-import { Link, Redirect } from 'react-router-dom';
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
-import { message, Card, Alert } from 'antd';
-import axios from 'axios';
-import _ from 'lodash';
+import { Redirect } from 'react-router-dom';
 import '../style/Login.css';
-import logo from '../images/logo/small_01.png';
 import BackgroundImage from '../components/BackgroundImage.jsx';
-const FormItem = Form.Item;
+import TopMiddleLogo from '../components/logo/TopMiddle.jsx';
+import LoginForm from './LoginForm.jsx';
 
-// Setup Alert Message Configuration
-message.config({
-    top: window.innerHeight * 10 / 100,
-    duration: 3,
-});
-
-class NormalLoginForm extends Component {
-    // Check to see if the entered username exists
-    checkExistingUsername = (rule, value, callback) => {
-        if (value) {
-            axios.get(`https://aquagrow.life/api/user/${value.toLowerCase()}`)
-                .then((res) => {
-                    if (res.data.error) {
-                        message.destroy();
-                        callback(`${value} is not an existing user 😢`);
-                    } else {
-                        message.destroy();
-                        message.success(`Hi, ${res.data.data.name}`);
-                        callback();
-                    }
-                })
-        } else {
-            callback();
-        }
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                // console.log('Received values of form: ', values);
-                this.props.authenticate(values, (authorized) => {
-                    if (authorized) {
-                        message.destroy();
-                        message.success("Welcome back! 🎉");
-                    } else {
-                        message.destroy();
-                        message.error("Your password is incorrect 😢");
-                    }
-                });
-            }
-        });
-    }
-
+class Login extends Component {
     render() {
-        const { getFieldDecorator } = this.props.form;
-        const validateUsername = _.debounce((rule, value, callback) => { this.checkExistingUsername(rule, value, callback) }, 300, {
-            'leading': false,
-            'trailing': true
-        });
-
         // If user is loged in (JWT stored in localStorage), redirect to homepage
         if (localStorage.getItem('jwt')) {
             return <Redirect to='/' />;
         }
 
         return (
-            <div>
+            <div className="login-container">
                 <BackgroundImage />
-                <div className='login-container'>
-                    <Card className="card-container">
-                        <div className='card-header'>
-                            <img src={logo} alt="" height="64" width="64" />
-                            <br></br><br></br>
-                        </div>
-                        <Form onSubmit={this.handleSubmit} className="login-form">
-                            <FormItem
-                                hasFeedback
-                            >
-                                {getFieldDecorator('username', {
-                                    rules: [{
-                                        required: true, message: 'Please input your username!'
-                                    }, {
-                                        validator: validateUsername
-                                    }],
-                                })(
-                                    <Input size="large" prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.5)' }} />} placeholder="Username" />
-                                )}
-                            </FormItem>
-                            <FormItem>
-                                {getFieldDecorator('password', {
-                                    rules: [{ required: true, message: 'Please input your Password!' }],
-                                })(
-                                    <Input size="large" prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.5)' }} />} type="password" placeholder="Password" />
-                                )}
-                            </FormItem>
-                            <FormItem>
-                                {getFieldDecorator('remember', {
-                                    valuePropName: 'checked',
-                                    initialValue: true,
-                                })(
-                                    <Checkbox>Remember me</Checkbox>
-                                )}
-                                <a className="login-form-forgot" href="">Forgot password?</a>
-                                <Button size="large" type="primary" htmlType="submit" className="login-form-button">
-                                    Log in
-          					    </Button>
-                            </FormItem>
-                        </Form>
-                        <div style={{ textAlign: 'center' }}>
-                            <br></br>New to AquaGrow?<br></br>
-                            <Link to="/register">Register now!</Link>
-                        </div>
-                    </Card>
+                <TopMiddleLogo />
+                <div className='login-form-container'>
+                    <h1>Log in with your account</h1>
+                    <LoginForm />
                 </div>
             </div >
         );
@@ -123,13 +27,9 @@ class NormalLoginForm extends Component {
 }
 
 function mapStateToProps(state) {
-    return { loggedIn: state.loggedIn };
+    return {
+        loggedIn: state.loggedIn
+    };
 }
 
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators({ authenticate, clearError }, dispatch);
-}
-
-const WrappedLoginForm = Form.create()(NormalLoginForm);
-
-export default connect(mapStateToProps, mapDispatchToProps)(WrappedLoginForm);
+export default connect(mapStateToProps)(Login);
