02f5830716bcfc00d6317b58af912e4ec793a631
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-04-10 04:44:57 -0500

    Update Dashboard Boilerplate

--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { logout, fetchUser } from '../actions/action_user';
-import { Button, Spin, Layout, Menu, Icon, message } from 'antd';
+import { Button, Spin, Layout, Menu, Icon, Avatar, Dropdown, message } from 'antd';
 import logo from '../images/logo/small_01.png'
 import '../style/Dashboard.css';
+// Test
+import { BrowserRouter, Router, Route, Switch } from 'react-router-dom';
+import FontAwesomeTest from '../components/FontAwesomeTest.jsx';
+import NotFound from '../components/404.jsx';
 const { Header, Sider, Content } = Layout;
 
 // Setup Alert Message Configuration
@@ -25,6 +29,10 @@ class SiderDemo extends Component {
 
         this.updateCollapsedWidth = this.updateCollapsedWidth.bind(this);
         this.selectMenuItem = this.selectMenuItem.bind(this);
+        this.handleLogout = this.handleLogout.bind(this);
+        this.handleMainContentClick = this.handleMainContentClick.bind(this);
+        this.handleMainContentClick = this.handleMainContentClick.bind(this);
+        this.handleAvatarMenuClick = this.handleAvatarMenuClick.bind(this);
     }
 
     componentWillMount() {
@@ -41,7 +49,7 @@ class SiderDemo extends Component {
     }
 
     // Logout and redirect to /login page
-    handleLogout = () => {
+    handleLogout() {
         this.props.logout(() => {
             this.props.history.push('/login');
         });
@@ -49,10 +57,11 @@ class SiderDemo extends Component {
 
     // If device is mobile, update collapsed width to 0
     updateCollapsedWidth() {
-        const deviceWidth = window.screen.width;
+        const deviceWidth = window.innerWidth;
         if (deviceWidth <= 576) {
             this.setState({
-                isMobile: true
+                isMobile: true,
+                collapsed: true
             })
         } else {
             this.setState({
@@ -74,6 +83,28 @@ class SiderDemo extends Component {
         }
     }
 
+    handleMainContentClick() {
+        if (this.state.isMobile && !this.state.collapsed) {
+            this.setState({
+                collapsed: true
+            })
+        }
+    }
+
+    handleAvatarMenuClick(values) {
+        if (values.key === "logout") {
+            this.handleLogout();
+        }
+    }
+
+    getMainContentStyle() {
+        if (this.state.isMobile && !this.state.collapsed) {
+            return { filter: "brightness(70%)" }
+        } else {
+            return { filter: "brightness(100%)" }
+        }
+    }
+
     toggle = () => {
         this.setState({
             collapsed: !this.state.collapsed,
@@ -81,6 +112,21 @@ class SiderDemo extends Component {
     }
 
     render() {
+        const avatarMenu = (
+            <Menu className="avatar-menu" onClick={this.handleAvatarMenuClick}>
+                <Menu.Item disabled>
+                    <Icon type="user" />Account Information
+                </Menu.Item>
+                <Menu.Item disabled>
+                    <Icon type="setting" />Account Settings
+                </Menu.Item>
+                <Menu.Divider />
+                <Menu.Item key="logout" style={{ color: "red" }} >
+                    <Icon type="logout" />Log out
+                </Menu.Item>
+            </Menu>
+        );
+
         return (
             <Layout className="layout-container">
                 <Sider
@@ -91,43 +137,72 @@ class SiderDemo extends Component {
                     collapsedWidth={this.state.isMobile ? 0 : 80}
                 >
                     <div className="logo" >
-                        <img src={logo} width="48" height="48" />
-                        {/* <span>AquaGrow</span> */}
+                        <Link to="/">
+                            <img src={logo} alt="" width="54" height="54" />
+                        </Link>
                     </div>
                     <Menu
-                        mode="vertical"
+                        mode="inline"
                         defaultSelectedKeys={this.state.selectedKeys}
                         selectedKeys={this.state.selectedKeys}
                         onClick={this.selectMenuItem}
+                    // style={{ fontSize: "30px" }}
                     >
                         <Menu.Item key="1">
-                            <Icon type="user" />
-                            <span>nav 1</span>
+                            <Link to="/">
+                                <Icon type="area-chart" />
+                                <span>Dashboard</span>
+                            </Link>
                         </Menu.Item>
                         <Menu.Item key="2">
-                            <Icon type="video-camera" />
-                            <span>nav 2</span>
+                            <Link to="/">
+                                <Icon type="video-camera" />
+                                <span>Portfolio</span>
+                            </Link>
                         </Menu.Item>
                         <Menu.Item key="3">
-                            <Icon type="upload" />
-                            <span>nav 3</span>
+                            <Link to="/">
+                                <Icon type="upload" />
+                                <span>Notifications</span>
+                            </Link>
                         </Menu.Item>
                     </Menu>
                 </Sider>
-                <Layout>
-                    <Header style={{ background: '#fff', padding: 0 }}>
-                        <Icon
-                            className="trigger"
-                            type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-                            onClick={this.toggle}
-                        />
+                <Layout
+                    className="content-container"
+                    onClick={this.handleMainContentClick}
+                    //TODO: update layout color based on state
+                    style={this.state.isMobile && !this.state.collapsed ? {
+                        filter: "brightness(70%)"
+                    } : {
+                            filter: "brightness(100%)"
+                        }}
+                >
+                    <Header className="header-container">
+                        <div className="header-left">
+                            <Icon
+                                className="trigger"
+                                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                                onClick={this.toggle}
+                            />
+                        </div>
+                        <div className="header-right">
+                            <Dropdown overlay={avatarMenu}>
+                                <Avatar
+                                    src={this.props.user ? this.props.user.pictureUrl : null}
+                                    style={{ backgroundColor: '#fff' }}
+                                    size="large"
+                                >
+                                    <Spin size="small" />
+                                </Avatar>
+                            </Dropdown>
+                        </div>
                     </Header>
                     <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
                         <div>
                             {this.props.user ? (
                                 <div>
-                                    <h1>Welcome to AquaGrow, {this.props.user.name}</h1>
-                                    <Button type="danger" onClick={this.handleLogout}>Log out</Button>
+                                    <h1>Welcome, {this.props.user.name}</h1>
                                 </div>
                             ) : (
                                     <Spin size="large" />
@@ -135,7 +210,7 @@ class SiderDemo extends Component {
                         </div>
                     </Content>
                 </Layout>
-            </Layout>
+            </Layout >
         );
     }
 }
