080f5790b125a59d13e9e5bc8bfc247edc4dfe81
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-04-29 23:08:58 -0500

    Add default value for sliders

--- src/containers/Notification/index.jsx
+++ src/containers/Notification/index.jsx
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux';
 import moment from 'moment';
 import { fetchNotifications, deleteNotification, clearNotifications } from '../../actions/action_notification.js';
 import { fetchUser } from '../../actions/action_user';
+import { fetchSystemStatus, updateSystemStatus } from '../../actions/action_system.js';
 import {
     Spin,
     Divider,
@@ -11,8 +12,16 @@ import {
     Col,
     List,
     Avatar,
-    Icon
+    Icon,
+    Button,
+    Switch,
+    Slider
 } from 'antd';
+import airTempLogo from '../../images/png/airTemp.png';
+import airHumidityLogo from '../../images/png/humidity.png';
+import lightIntensityLogo from '../../images/png/growLight.png';
+import waterTempLogo from '../../images/png/waterTemp.png';
+import phLevelLogo from '../../images/png/pH.png';
 import './Notification.css';
 
 const icons = {
@@ -27,24 +36,208 @@ const iconBackground = {
     success: "#52c41a",
 }
 
+const airTempMarks = {
+    0: {
+        style: {
+            color: '#40a9ff',
+        },
+        label: "0°C"
+    },
+    10: '10°C',
+    20: '20°C',
+    30: '30°C',
+    40: '40°C',
+    50: {
+        style: {
+            color: '#f50',
+        },
+        label: "50°C",
+    }
+}
+
+const airHumidityMarks = {
+    0: {
+        style: {
+            color: '#40a9ff',
+        },
+        label: "0%"
+    },
+    25: '25%',
+    50: '50%',
+    75: '35%',
+    100: {
+        style: {
+            color: '#f50',
+        },
+        label: "100%",
+    }
+}
+
+const lightIntensityMarks = {
+    0: {
+        style: {
+            color: '#40a9ff',
+        },
+        label: "0 lux"
+    },
+    25000: '25k lux',
+    50000: '50k lux',
+    65000: {
+        style: {
+            color: '#f50',
+        },
+        label: "65k lux",
+    }
+}
+
+const waterTempMarks = {
+    0: {
+        style: {
+            color: '#40a9ff',
+        },
+        label: "0°C"
+    },
+    10: '10°C',
+    20: '20°C',
+    30: '30°C',
+    40: '40°C',
+    50: {
+        style: {
+            color: '#f50',
+        },
+        label: "50°C",
+    }
+}
+
+const phLevelMarks = {
+    0: {
+        style: {
+            color: '#f5222d',
+        },
+        label: "0"
+    },
+    1: {
+        style: {
+            color: '#fa541c',
+        },
+        label: "1"
+    },
+    2: {
+        style: {
+            color: '#fa8c16',
+        },
+        label: "2"
+    },
+    3: {
+        style: {
+            color: '#faad14',
+        },
+        label: "3"
+    },
+    4: {
+        style: {
+            color: '#fadb14',
+        },
+        label: "4"
+    },
+    5: {
+        style: {
+            color: '#a0d911',
+        },
+        label: "5"
+    },
+    6: {
+        style: {
+            color: '#a0d911',
+        },
+        label: "6"
+    },
+    7: {
+        style: {
+            color: '#52c41a',
+        },
+        label: "7"
+    },
+    8: {
+        style: {
+            color: '#237804',
+        },
+        label: "8"
+    },
+    9: {
+        style: {
+            color: '#13c2c2',
+        },
+        label: "9"
+    },
+    10: {
+        style: {
+            color: '#1890ff',
+        },
+        label: "10"
+    },
+    11: {
+        style: {
+            color: '#2f54eb',
+        },
+        label: "11"
+    },
+    12: {
+        style: {
+            color: '#1d39c4',
+        },
+        label: "12"
+    },
+    13: {
+        style: {
+            color: '#722ed1',
+        },
+        label: "13"
+    },
+    14: {
+        style: {
+            color: '#531dab',
+        },
+        label: "14"
+    }
+}
+
 class Notifications extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            loadingNotifications: true
+            loadingNotifications: true,
+            loadingNotificationSettings: true,
+            airTempRange: [0, 0],
+            airHumidityRange: [0, 0],
+            lightIntensityRange: [0, 0],
+            waterTempRange: [0, 0],
+            phLevelRange: [0, 0]
         }
         this.onDeleteItemClick = this.onDeleteItemClick.bind(this);
+        this.handleAirTempRangeChange = this.handleAirTempRangeChange.bind(this);
     }
 
     componentWillMount() {
         const currentUser = localStorage.getItem('username');
         this.props.fetchNotifications(currentUser);
+        this.props.fetchSystemStatus(currentUser);
     }
 
     componentDidUpdate() {
         if (this.state.loadingNotifications === true) {
             this.setState({
                 loadingNotifications: false
+            })
+        }
+        if (this.state.loadingNotificationSettings === true && this.props.system) {
+            this.setState({
+                loadingNotificationSettings: false,
+                airTempRange: this.props.system.airTempRange,
+                airHumidityRange: this.props.system.airHumidityRange,
+                lightIntensityRange: this.props.system.lightIntensityRange,
+                waterTempRange: this.props.system.waterTempRange,
+                phLevelRange: this.props.system.phLevelRange
             })
         }
     }
@@ -58,36 +251,169 @@ class Notifications extends Component {
         })
     }
 
+    handleAirTempRangeChange(value) {
+        this.setState({
+            airTempRange: value
+        })
+    }
+
     render() {
         return (
             <div>
                 <Divider>System Notifications</Divider>
                 <Row>
                     <Col xs={0} sm={0} md={0} lg={0} xl={3} span={3} ></Col>
-                    <Col xs={24} sm={24} md={24} lg={24} xl={18} span={18} className="notification-container">
-                        <h2>Notifications ({this.props.notifications.length})</h2>
-                        <Divider />
-                        <List
-                            loading={this.state.loadingNotifications}
-                            itemLayout="horizontal"
-                            dataSource={this.props.notifications}
-                            renderItem={item => (
-                                <List.Item actions={[<div onClick={() => this.onDeleteItemClick(item._id)}><Icon type="close-circle-o" /></div>]}>
+                    <Col xs={24} sm={24} md={24} lg={24} xl={18} span={18}>
+                        <div className="notification-container">
+                            <h2>Notifications ({this.props.notifications.length})</h2>
+                            <Divider />
+                            <List
+                                loading={this.state.loadingNotifications}
+                                itemLayout="horizontal"
+                                dataSource={this.props.notifications}
+                                renderItem={item => (
+                                    <List.Item actions={[<div onClick={() => this.onDeleteItemClick(item._id)}><Icon type="close-circle-o" /></div>]}>
+                                        <List.Item.Meta
+                                            avatar={<Avatar
+                                                style={{ backgroundColor: iconBackground[item.type], fontWeight: "900" }}
+                                                icon={icons[item.type]}
+                                            />}
+                                            title={`[${item.aspect.toUpperCase()}] ${item.message}`}
+                                            description={moment(item.createdAt).format("HH:mm:ss (ddd, MMM DD)")}
+                                        />
+                                    </List.Item>
+                                )}
+                            />
+                        </div>
+
+                        <div className="notification-setting-container">
+                            <br></br>
+                            <h2>Notifications Settings</h2>
+                            <Divider />
+                            <p>Adjust the range of the following criteria. We will monitor the system and
+                            send you notifications when the sensor data fall out of the set range.</p>
+                            <List
+                                itemLayout="horizontal"
+                            >
+                                {/* AIR TEMPERATURE RANGE */}
+                                <List.Item>
                                     <List.Item.Meta
                                         avatar={<Avatar
-                                            style={{ backgroundColor: iconBackground[item.type], fontWeight: "900" }}
-                                            icon={icons[item.type]}
+                                            size="large"
+                                            src={airTempLogo}
+                                            style={{ fontWeight: "900" }}
                                         />}
-                                        title={`[${item.aspect.toUpperCase()}] ${item.message}`}
-                                        description={moment(item.createdAt).format("HH:mm:ss (ddd, MMM DD)")}
+                                        title={`Air temperature range`}
+                                        description={
+                                            <div>
+                                                <p>Current set range is {this.props.system ? this.props.system.airTempRange[0] + "°C - " + this.props.system.airTempRange[1] + "°C" : "loading..."}</p>
+                                                <Slider
+                                                    min={0}
+                                                    max={50}
+                                                    range
+                                                    step={0.1}
+                                                    marks={airTempMarks}
+                                                    value={this.state.airTempRange}
+                                                    onChange={this.handleAirTempRangeChange}
+                                                ></Slider>
+                                            </div>
+                                        }
                                     />
                                 </List.Item>
-                            )}
-                        />
-
-                        <br></br>
-                        <h2>Notifications Settings</h2>
-                        <Divider />
+                                {/* AIR HUMIDITY RANGE */}
+                                <List.Item>
+                                    <List.Item.Meta
+                                        avatar={<Avatar
+                                            size="large"
+                                            src={airHumidityLogo}
+                                            style={{ fontWeight: "900" }}
+                                        />}
+                                        title={"Air humidity range"}
+                                        description={
+                                            <Slider
+                                                min={0}
+                                                max={100}
+                                                range
+                                                step={0.1}
+                                                marks={airHumidityMarks}
+                                                value={this.state.airHumidityRange}
+                                            ></Slider>
+                                        }
+                                    />
+                                </List.Item>
+                                {/* LIGHT INTENSITY RANGE */}
+                                <List.Item>
+                                    <List.Item.Meta
+                                        avatar={<Avatar
+                                            size="large"
+                                            src={lightIntensityLogo}
+                                            style={{ fontWeight: "900" }}
+                                        />}
+                                        title={"Light intensity range"}
+                                        description={
+                                            <Slider
+                                                min={0}
+                                                max={65000}
+                                                range
+                                                step={1000}
+                                                marks={lightIntensityMarks}
+                                                value={this.state.lightIntensityRange}
+                                            ></Slider>
+                                        }
+                                    />
+                                </List.Item>
+                                {/* WATER TEMPERATURE RANGE */}
+                                <List.Item>
+                                    <List.Item.Meta
+                                        avatar={<Avatar
+                                            size="large"
+                                            src={waterTempLogo}
+                                            style={{ fontWeight: "900" }}
+                                        />}
+                                        title={"Water temperature range"}
+                                        description={
+                                            <Slider
+                                                min={0}
+                                                max={50}
+                                                range
+                                                step={0.1}
+                                                marks={waterTempMarks}
+                                                value={this.state.waterTempRange}
+                                            ></Slider>
+                                        }
+                                    />
+                                </List.Item>
+                                {/* PH LEVEL RANGE */}
+                                <List.Item>
+                                    <List.Item.Meta
+                                        avatar={<Avatar
+                                            size="large"
+                                            src={phLevelLogo}
+                                            style={{ fontWeight: "900" }}
+                                        />}
+                                        title={"pH level range"}
+                                        description={
+                                            <Slider
+                                                min={0}
+                                                max={14}
+                                                range
+                                                step={0.1}
+                                                marks={phLevelMarks}
+                                                value={this.state.phLevelRange}
+                                            ></Slider>
+                                        }
+                                    />
+                                </List.Item>
+                            </List>
+                            <div style={{ textAlign: "center", paddingTop: "24px" }}>
+                                <Button
+                                    size="large"
+                                    type="primary"
+                                >
+                                    Set threshold values
+                                </Button>
+                            </div>
+                        </div>
                     </Col>
                     <Col xs={0} sm={0} md={0} lg={0} xl={3} span={3} ></Col>
                 </Row>
@@ -99,7 +425,8 @@ class Notifications extends Component {
 function mapStateToProps(state) {
     return {
         user: state.user,
-        notifications: state.notifications
+        notifications: state.notifications,
+        system: state.system
     };
 }
 
@@ -108,7 +435,9 @@ function mapDispatchToProps(dispatch) {
         fetchNotifications,
         deleteNotification,
         clearNotifications,
-        fetchUser
+        fetchUser,
+        fetchSystemStatus,
+        updateSystemStatus
     }, dispatch);
 }
 
